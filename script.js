572dbd78840de7c471a227243137adfc4f528633
DemetreBerdzenadze <demeberdzenadze@gmail.com>
2023-11-19 17:56:44 +0400

    finished

---
+++ script.js
@@ -0,0 +1,98 @@
+// TASK 1
+// for(let i = 0; i <= 100; i++) {
+//   console.log(i);
+// }
+
+
+// TASK 2 
+// let array1= [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12];
+
+// for(let i = 0; i < array1.length; i++) {
+
+// }
+
+
+// TASK3
+// let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 8];
+
+// for(let i = 0; i <= array2.length; i++) {
+//   if(i == 5){
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+
+// TASK 4
+// let array3= [1, 2, 3, 4, 5];
+
+// let sum = 0
+
+// for(let i = 0; i < array3.length; i++){
+//   sum += array3[i];
+// }
+
+// console.log(sum);
+
+
+// TASK 5
+// let array4 = [1, 2, 3, 7, 6, 9];
+
+// let sum2 = 0
+
+// for(let i = 0; i < array4.length; i++) {
+//   sum2 += array4[i];
+// }
+// console.log(sum2 / 6);
+
+
+// // TASK6
+// let array5 = [1, 2, 3, 7, 6, 9];
+
+// for(let i = 0; i < array5.length; i++) {
+//   if(i == 7){
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// TASK 7
+// let user = {
+//   firstname: "giorgi",
+//   lastname: "smith",
+//   age: 25,
+//   studentstatus: "active"
+// };
+
+// console.log(user.studentstatus);
+
+
+// TASK 8
+// let user = {
+//   name: 'giorgi',
+//   age: 20,
+//   studentstatus: 'active'
+// }
+
+// if(user.age < 18 && user.studentstatus == "active") {
+//   console.log("hello");
+// }
+// else if (user.name == "levani"){
+//   console.log("hellow levani");
+// }
+// else if (user.studentstatus == "active" && user.age < 25){
+//   console.log("Hellow World");
+// }
+// else{
+//   console.log(Error);
+// }
+
+
+// TASK11
+// let users = [
+//   {username: 'giorgi', status: false},
+//   {username: 'levani', status: false},
+//   {username: 'anna', status: true}
+// ]
+
+// console.log();
